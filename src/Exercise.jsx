d7a22e1fcd1e20e9b390e79525de3c93126ae534
AsifArkadia <kangasmaaara22@gmail.com>
2025-11-01 21:47:58 +0700

    Understanding JSX(2)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,35 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      console.error("[Component] Gagal menampilkan data:", error.message);
+    }
+  };
+  fetchData();
+}, []);
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard
-            name="Asif"
-            email="asif@gmail.com"
-            street="Jl. Imam Bonjol"
-            city="Semarang"
-          />
-          <UserCard 
-            name="Maulida"
-            email="maulida@gmail.com"
-            street="Jl. Indrapasta"
-            city="Demak"
-          />
-          <UserCard 
-            name="Arkadia"
-            email="arkadia@gmail.com"
-            street="Jl. Pandanaran"
-            city="Kudus"
-          />
-        </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        User Cards
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+        {users.length > 0 ? (
+          users.map((user) => <UserCard key={user.email} {...user} />)
+        ) : (
+          <p className="text-center text-gray-500 col-span-full">
+            Memuat data pengguna...
+          </p>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 

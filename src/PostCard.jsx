8b782c2c2993fdfe6ce90f998a0ee5e34ad41064
AsifArkadia <kangasmaaara22@gmail.com>
2025-11-25 10:29:33 +0700

    Review UTS

--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -1,28 +1,30 @@
 import React, {useState} from 'react'
 
 function PostCard(props) {
-   const{id, userId, title, body,...rest} = props;
-   const [clicked, setClicked] = useState(false);
+  const { id, userId, title, body } = props;
+  const [clicked, setClicked] = useState(false);
 
-   console.log(Object.entries(rest));
+  return (
+    <div className="bg-white hover:bg-red-50 p-6 rounded-lg shadow hover:shadow-lg hover:border transition-shadow text-center flex flex-col justify-between hover:scale-105">
+      <div>
+        <h2 className="text-xl font-semibold text-gray-800 mb-2 capitalize">
+          {title}
+        </h2>
+        <p className="text-gray-600">{body}</p>
+      </div>
 
-    return (
-    <div className="bg-white p-6 rounded-lg shadow hover:bg-red-500 transition-shadow">
-      <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
-      <p className="text-gray-600">
-        <span className="font-medium"></span> {body}
-      </p>
-            {/* Menampilkan data tambahan dari rest */}
-      {Object.entries(rest).map(([key, value]) => (
-        <p key={key} className="text-gray-600">
-          <span className="font-medium capitalize">{key}:</span> {value}
-        </p>
-      ))}
-      <button className={`${clicked ? "bg-special-red2" : "bg-gray-01"} text-white p-2 rounded-md`}
-      onClick={() => setClicked(true)}
+      <button
+        className={`text-white p-2 rounded-md mt-2 
+            ${
+              clicked
+                ? "bg-red-700 hover:bg-special-red "
+                : "bg-gray-01 hover:bg-gray-02"
+            }
+          `}
+        onClick={() => setClicked(true)}
       >
         {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
-</button>
+      </button>
     </div>
   );
 }
